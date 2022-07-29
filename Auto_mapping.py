15d1686da49915df3c0a773b8fa934cca89b481f
tankren <tankren@users.noreply.github.com>
2022-07-29 23:54:19 +0800

    代码优化

--- Auto_mapping.py
+++ Auto_mapping.py
@@ -1,14 +1,18 @@
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
-#importing the module 
-import logging, os, sys, subprocess, time
-
+from lxml import etree
+#importing other modules
+import logging, os, sys, subprocess, time, datetime
 os.popen("chcp 936")
+
+start=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+logfile='处理日志_' + start + '.txt'
+
 
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 #now we will Create and configure logger 
-logging.basicConfig(filename="处理结果.txt", 
+logging.basicConfig(filename='处理日志.txt', 
 					format='%(asctime)s - %(levelname)s: %(message)s', 
 					filemode='w',
                     encoding='utf-8', 
@@ -21,22 +25,18 @@ Tk().withdraw() # we don't want a full GUI, so keep the root window from appeari
 
 xmlfile = askopenfilename(filetypes=[('XML文件', '.xml')],title='选择金税系统导出XML文件') # show an "Open" dialog box and return the path to the selected file
 txtfile = askopenfilename(filetypes=[('TXT文件', '.txt')],title='选择批量导入结果TXT文件')
-
-
+print('XML文件为:',xmlfile)
+print('TXT文件为:',txtfile)    
+logger.info(f'选择的XML文件为: {xmlfile}')
+logger.info(f'选择的TXT文件为: {txtfile}')
 
 if xmlfile == '' or txtfile == '':
-    print('没有选择文件,程序将退出')
-    logger.warning('没有选择文件,程序将退出')
+    print('文件选择有误,程序将退出,请重新运行')
+    logger.warning('文件选择有误,程序将退出,请重新运行')
     logging.shutdown()
-    os.system('处理结果.txt')
+    os.rename('处理日志.txt', logfile)
+    subprocess.Popen(['notepad.exe', logfile])
 else:    
-    print('XML文件为:',xmlfile)
-    print('TXT文件为:',txtfile)    
-    logger.info(f'选择的XML文件为: {xmlfile}')
-    logger.info(f'选择的TXT文件为: {txtfile}')
-    #import xml.etree.ElementTree as ET
-    from lxml import etree
-    
     xml = etree.parse(xmlfile) #读取xml文件
     root = xml.getroot() #获取根节点
     
@@ -57,16 +57,21 @@ else:
                     if fphm.text in line:
                         import re
                         pattern = re.compile(r"\d{10}")
-                        map =pattern.findall(line)[0]
-                        print('匹配到的单据号为:',map,'修改XML单据号...')
-                        logger.info(f'匹配到的单据号为: {map} 修改XML单据号...')
-                        djh.text = map
-                        #newxml=xmlfile+'_new.xml'
-                        xml.write(xmlfile,pretty_print=True)
-                        print('修改成功!!')
-                        logger.info('修改成功!!')
+                        map = pattern.findall(line)[0]
+                        if map == '':
+                            print('发票号码',fphm.text,'未匹配到单据号')
+                            logger.info(f'发票号码{fphm.text}未匹配到单据号')
+                        else:
+                            print('匹配到的单据号为:',map,'修改XML单据号...')
+                            logger.info(f'匹配到的单据号为: {map} 修改XML单据号...')
+                            djh.text = map
+                            #newxml=xmlfile+'_new.xml'
+                            xml.write(xmlfile,pretty_print=True)
+                            print('修改成功!!')
+                            logger.info('修改成功!!')
     logging.shutdown()
-    subprocess.Popen(['notepad.exe','处理结果.txt'])
+    os.rename('处理日志.txt', logfile)
+    subprocess.Popen(['notepad.exe', logfile])
     subprocess.Popen(['C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe',xmlfile])
     time.sleep(5)
     sys.exit()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GoldenTax_Djh_Autofill
 Autofill the Djh in xml exported from Golden tax
+一个小工具，用来自动补全发票号对应的单据号
